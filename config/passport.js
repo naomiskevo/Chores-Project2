09f1a14be350d4c726dba7ecb5e5515f87942f8b
naomiskevo <naomiskevo@gmsail.com>
2019-11-26 15:22:38 -0600

    initializes passport and OAuth

---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const Lady = require('../models/lady');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    Lady.findOne({ 'googleId': profile.id }, function(err, student) {
+      if (err) return cb(err);
+      if (lady) {
+        return cb(null, lady);
+      } else {
+        // we have a new student via OAuth!
+        var newLady = new Lady({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          googleId: profile.id
+        });
+        newLady.save(function(err) {
+          if (err) return cb(err);
+          return cb(null, newLady);
+        });
+      }
+    });
+  }
+));
+
+passport.serializeUser(function(lady, done) {
+    done(null, lady.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    Lady.findById(id, function(err, lady) {
+      done(err, lady);
+    });
+  });
--- server.js
+++ server.js
@@ -3,23 +3,35 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+
+require('dotenv').config();
+
+var app = express();
+
+require('./config/database');
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-require('./config/database');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'SEIRocks!',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
