542da3e7d54b3d47680f73ad4016ab5c12c359cd
naomiskevo <naomiskevo@gmsail.com>
2019-11-26 14:20:43 -0600

    install mongoose and connect mongoDb Atlas

---
+++ config/database.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+db.on('connected', function () {
+    console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
+});
+   
+module.exports = mongoose;
